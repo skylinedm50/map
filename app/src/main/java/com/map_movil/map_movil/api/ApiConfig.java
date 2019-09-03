f1de5d8e688b41c5cb574bafa4d80c6d7ff010a9
Jorge Espinal <espinalj49@gmail.com>
2019-09-03 14:37:00 -0600

    Cambios realizados para poder subir la aplicación a la APP Store, nueva IP asignada a la institución.

--- app/src/main/java/com/map_movil/map_movil/api/ApiConfig.java
+++ app/src/main/java/com/map_movil/map_movil/api/ApiConfig.java
@@ -1,8 +1,8 @@
 package com.map_movil.map_movil.api;
 
 public class ApiConfig {
-    private String strIpServer = "190.6.200.98";
-    public static final String strURL = "http://190.6.200.98:3004/index.php/"; //192.168.41.8
-    public static final String strURLMap = "http://190.6.200.98:3004"; //192.168.41.8
-    public static final String strURLDocument = "http://190.6.200.98:82"; //192.168.41.8
+    private String strIpServer = "143.208.18.66";
+    public static final String strURL = "http://143.208.18.66:3004/index.php/"; //192.168.41.8
+    public static final String strURLMap = "http://143.208.18.66:3004"; //192.168.41.8
+    public static final String strURLDocument = "http://143.208.18.66:82"; //192.168.41.8
 }
